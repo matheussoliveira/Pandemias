22d718ea23d41fc860373f14bede650874d3c6a6
Matheus de Souza Oliveira <m203407@dac.unicamp.br>
2020-05-19 08:47:30 -0300

    'Visao Geral' ui done

---
+++ oliveiras-bot/View/VisaoGeralTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  VisaoGeralTableViewCell.swift
+//  oliveiras-bot
+//
+//  Created by Matheus Oliveira on 15/05/20.
+//  Copyright © 2020 Oliveiras. All rights reserved.
+//
+
+import UIKit
+
+class VisaoGeralTableViewCell: UITableViewCell {
+    
+    // Labels
+    @IBOutlet weak var confirmedNumber: UILabel!
+    @IBOutlet weak var recoveredNumber: UILabel!
+    @IBOutlet weak var activeNumber: UILabel!
+    @IBOutlet weak var deathsNumber: UILabel!
+    
+    // Views
+    @IBOutlet weak var confirmedView: UIView!
+    @IBOutlet weak var recoveredView: UIView!
+    @IBOutlet weak var activeView: UIView!
+    @IBOutlet weak var deathsView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layoutIfNeeded()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
