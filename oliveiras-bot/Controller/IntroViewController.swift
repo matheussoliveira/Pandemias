3d50c9d85bbb72bae6e4b6c8ad82bb0cac0be281
Marina Miranda Aranha <ra174233@students.ic.unicamp.br>
2020-05-29 09:29:11 -0300

    fix corrupted file

--- oliveiras-bot/Controller/IntroViewController.swift
+++ oliveiras-bot/Controller/IntroViewController.swift
@@ -32,19 +32,19 @@ class IntroViewController: UIViewController{
         
     }
     
-    var pages : [View] {
+    var pages : [IntroView] {
         get {
-            let page1: View = Bundle.main.loadNibNamed("View", owner: self, options: nil)?.first as! View
+            let page1: IntroView = Bundle.main.loadNibNamed("IntroView", owner: self, options: nil)?.first as! IntroView
             page1.imageView.image = UIImage(named: "intro-chat")
             page1.title.text = "Conversa"
             page1.descriptionLabel.text = "Tire suas dúvidas e receba dicas sobre o Coronavírus (COVID - 19), com fontes confiáveis através de uma conversa com June"
             
-            let page2: View = Bundle.main.loadNibNamed("View", owner: self, options: nil)?.first as! View
+            let page2: IntroView = Bundle.main.loadNibNamed("IntroView", owner: self, options: nil)?.first as! IntroView
             page2.imageView.image = UIImage(named: "intro-stats")
             page2.title.text = "Estatísticas"
             page2.descriptionLabel.text = "Obtenha estatísticas atualizadas sobre os casos confirmados, recuperados, mortes e ativos, através de gráficos e uma visão geral"
             
-            let page3: View = Bundle.main.loadNibNamed("View", owner: self, options: nil)?.first as! View
+            let page3: IntroView = Bundle.main.loadNibNamed("IntroView", owner: self, options: nil)?.first as! IntroView
             page3.imageView.image = UIImage(named: "intro-faq")
             page3.title.text = "Dúvidas"
             page3.descriptionLabel.text = "Acesse respostas sobre dúvidas frequentes a respeito do Coronavírus (COVID - 19) de maneira rápida e objetiva com fontes confiáveis"
@@ -69,7 +69,7 @@ class IntroViewController: UIViewController{
             pageControl.currentPage = 0
         }
 
-        func setupScrollView(pages: [View]) {
+        func setupScrollView(pages: [IntroView]) {
             scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
             scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(pages.count), height: view.frame.height)
             scrollView.isPagingEnabled = true
