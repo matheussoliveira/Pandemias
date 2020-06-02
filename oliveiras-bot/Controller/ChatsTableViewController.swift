57bedc338c922b515caf62b46a5b7cdde71d91b5
Julia Mestre <juliacmestre@gmail.com>
2020-06-02 11:21:33 -0300

    Refined the Chats View

---
+++ oliveiras-bot/Controller/ChatsTableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ChatsTableViewController.swift
+//  oliveiras-bot
+//
+//  Created by Julia Conti Mestre on 02/06/20.
+//  Copyright © 2020 Oliveiras. All rights reserved.
+//
+
+import UIKit
+
+class ChatsTableViewController: UITableViewController {
+    
+    @IBOutlet weak var chatImage: UIImageView!
+    @IBOutlet weak var chatName: UILabel!
+    @IBOutlet weak var chatSubtitle: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        chatImage.layer.cornerRadius = chatImage.frame.width/2
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        
+        if indexPath.section == 0 && indexPath.row == 0 {
+            let chevron = UIImage(named: "chevron-icon")
+            cell.accessoryType = .disclosureIndicator
+            cell.accessoryView = UIImageView(image: chevron!)
+            chatImage.image = UIImage(named: "june")
+            chatName.text = "June"
+            chatSubtitle.text = "Coronavírus (Covid - 19)"
+        }
+        
+
+        return cell
+    }
+
+    
+}
