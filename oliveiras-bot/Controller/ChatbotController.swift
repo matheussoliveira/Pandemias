c0084092aaf3d4b6076be7d24115a133f6c9974f
Matheus de Souza Oliveira <m203407@dac.unicamp.br>
2020-06-02 11:31:28 -0300

    Adding style to chat text input

--- oliveiras-bot/Controller/ChatbotController.swift
+++ oliveiras-bot/Controller/ChatbotController.swift
@@ -164,14 +164,13 @@ class ChatbotController: UIViewController{
                    
         NotificationCenter.default.addObserver(self, selector: #selector(self.handleKeyBoardNotification), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.handleKeyBoardNotification), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
     }
     
     func setUpTextField(){
-        inputTextField.layer.cornerRadius = 18
-        inputTextField.layer.borderWidth = 0.8
-        inputTextField.layer.borderColor = UIColor(red: 0.78, green: 0.78, blue: 0.80, alpha: 1.00).cgColor
-        inputTextField.attributedPlaceholder = NSAttributedString(string: "Faça uma pergunta ao bot",attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 0.78, green: 0.78, blue: 0.80, alpha: 1.00)])
+        self.containerView.layer.borderWidth = 0.8
+        self.containerView.layer.borderColor = UIColor.white.cgColor
+        self.containerView.layer.cornerRadius = 22
+        inputTextField.attributedPlaceholder = NSAttributedString(string: "Faça uma pergunta à June",attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 0.78, green: 0.78, blue: 0.80, alpha: 1.00)])
     }
     
     @objc func handleKeyBoardNotification(notification: NSNotification){
